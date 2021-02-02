0a87b307c12d1581ca7f11751f541fa5753a39fc
PablloS <67272684+PablloS@users.noreply.github.com>
2021-02-02 12:46:13 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(document).ready( function() {
         $('.header__burger, .nav').toggleClass('active'); 
         $('body').toggleClass('lock'); 
     });
+
+    //плавная анимация прокручивания страницы по ссылке
+    $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href'); 
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500); 
+    });
+
 });
