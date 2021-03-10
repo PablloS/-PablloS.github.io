41b05d9a6810dc8b1615172f098720af62dc73ce
PablloS <67272684+PablloS@users.noreply.github.com>
2021-03-10 14:02:04 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,4 +14,41 @@ $(document).ready( function() {
         }, 500); 
     });
 
+    //анимация появляющихся объектов
+
+    const animateItems = $('.anim_items'); 
+
+    if (animateItems.length>0) {
+        window.addEventListener('scroll', amimOnscr);
+        function amimOnscr() {
+            for (let index = 0; index < animateItems.length; index++) {
+                const animItem = animateItems[index];
+                const animHeight = animItem.offsetHeight; 
+                const animItemOffset = offset(animItem).top; 
+                const animStart = 4; 
+
+                let animItemPoint = window.innerHeight - animHeight / animStart; 
+                if (animHeight > window.innerHeight) {
+                    let animItemPoint = window.innerHeight - window.innerHeight /  animStart; 
+                }
+
+                if ((pageYOffset>animItemOffset-animItemPoint) && pageYOffset<(animItemOffset+animHeight)) {
+                    animItem.classList.add('active'); 
+                }
+                
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(), 
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top+scrollTop, left: rect.left+scrollLeft}
+    }
+
+    setTimeout(() => {
+        amimOnscr();
+    }, 300);
+
 });
